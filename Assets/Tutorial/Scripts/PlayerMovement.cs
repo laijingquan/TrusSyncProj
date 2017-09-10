488c6ad63e0447d3f341e8b6ed6bc60eef481fa3
laijingquan <351779036@qq.com>
2017-09-10 22:46:48 +0800

    第一次提交

---
+++ Assets/Tutorial/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using TrueSync;
+
+public class PlayerMovement : TrueSyncBehaviour {
+
+
+    public override void OnSyncedInput()
+    {
+        FP accell = Input.GetAxis("Vertical");
+        FP steer = Input.GetAxis("Horizontal");
+        TrueSyncInput.SetFP(0, accell);
+        TrueSyncInput.SetFP(1, steer);
+
+    }
+
+    public override void OnSyncedUpdate()
+    {
+        FP accell = TrueSyncInput.GetFP(0);
+        FP steer = TrueSyncInput.GetFP(1);
+
+        accell *= 10 * TrueSyncManager.DeltaTime;
+        steer *= 250 * TrueSyncManager.DeltaTime;
+
+        tsTransform.Translate(0, 0, accell, Space.Self);
+        tsTransform.Rotate(0, steer, 0);
+    }
+}
---
+++ Assets/Tutorial/Scripts/TutorialMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialMenu : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("v1.0");
+        PhotonNetwork.automaticallySyncScene = true;
+	}
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("room1", null, null);
+    }
+    // Update is called once per frame
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "players" + PhotonNetwork.playerList.Length);
+        if(PhotonNetwork.isMasterClient&&GUI.Button(new Rect(10,40,100,30),"start"))
+        {
+            PhotonNetwork.LoadLevel("Tutorial/Game");
+        }
+    }
+}
